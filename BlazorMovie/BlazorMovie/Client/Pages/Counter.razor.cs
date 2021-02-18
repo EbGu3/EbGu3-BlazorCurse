7643b25deec13cabfed33bfc0151140520bb1347
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-02-18 17:34:03 -0600

    Divide the code C# and the UI

---
+++ BlazorMovie/BlazorMovie/Client/Pages/Counter.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorMovie.Client.Servicio;
+
+namespace BlazorMovie.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject]
+        ServiceSingleton singleton { get; set; }
+
+        [Inject]
+        ServiceTransient transient { get; set; }
+
+        private int currentCount = 0;
+        private void IncrementCount()
+        {
+            currentCount++;
+            singleton.Value = currentCount;
+            transient.Value = currentCount;
+        }
+    }
+}
