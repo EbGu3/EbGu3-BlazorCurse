b9158bd52da0ac89a38eb700be7ef006b90601e1
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-02-19 09:34:44 -0600

    Use javascript

---
+++ BlazorMovie/BlazorMovie/Client/Models/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovie.Client.Models
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        //Se utiliza InvokeAsync para retornar algo
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+        {
+            return await js.InvokeAsync<bool>("confirm", message);
+        }
+
+        public static async ValueTask Log(this IJSRuntime js)
+        {
+            await js.InvokeVoidAsync("console.log", "prueba de console log");
+        }
+    }
+}
