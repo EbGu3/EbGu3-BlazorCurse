35ac0dc28d58c10f52901c209e1327d39c38eb2b
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-02-22 12:34:00 -0600

    Formulario Pelicula

--- BlazorMovie/BlazorMovie/Shared/Entity/Movie.cs
+++ BlazorMovie/BlazorMovie/Shared/Entity/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,18 @@ namespace BlazorMovie.Shared.Entity
 {
     public class Movie
     {
+        [Required]
         public string Title { get; set; }
-        public DateTime LaunchDate { get; set; }
-        public string Poster { get; set; }
 
+        [Required]
+        public DateTime? LaunchDate { get; set; }
+
+        public int Id { get; set; }
+        public string  Summary { get; set; }
+        public bool OnBilldBoard { get; set; }
+        public string Trailer { get; set; }
+ 
+        public string Poster { get; set; }
         public string CutTitle
         {
             get
