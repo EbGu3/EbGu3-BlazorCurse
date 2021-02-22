0125e9f97d6b6251634511315be89b2af30f4331
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-02-22 00:58:19 -0600

    Fomularios Genericos

--- BlazorMovie/BlazorMovie/Shared/Entity/Gender.cs
+++ BlazorMovie/BlazorMovie/Shared/Entity/Gender.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BlazorMovie.Shared.Entity
 {
     public class Gender
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Name { get; set; }
     }
 }
