dfb900279f68233402e34dca5c2a4466fece1645
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2021-02-22 08:04:50 -0600

    Creando Formulario Date

---
+++ BlazorMovie/BlazorMovie/Shared/Entity/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMovie.Shared.Entity
+{
+    public class Person
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime? DateBorn { get; set; }
+
+        public int Id { get; set; }
+        public string Biografia { get; set; }
+        public string Photo { get; set; }
+
+    }
+}
